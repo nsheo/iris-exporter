888ce151d350d657152a15d736b6e0ff70e67c91
nsheo <nsheo@ntels.com>
2020-09-24 14:20:53 +0900

    iris exporter service script edit

--- iris-exporter.sh
+++ iris-exporter.sh
@@ -2,7 +2,7 @@
 cat << EOF > ${PWD}/iris-exporter.service
 [Unit]
 Description=Prometheus iris exporter
-ConditionPathExists=${PWD}/iris-exporter
+ConditionPathExists=${HOME}/iris-exporter/iris-exporter
 After=network.target
 
 [Service]
@@ -10,11 +10,13 @@ Type=simple
 LimitNOFILE=1024
 Restart=on-failure
 RestartSec=10
-WorkingDirectory=${PWD}
-ExecStart=${PWD}/iris-exporter -sedfile=${PWD}/sedcommand.file
+AmbientCapabilities=CAP_SYS_CHROOT
+User=iris
+Group=iris
+RootDirectoryStartOnly=true
+WorkingDirectory=${HOME}
+ExecStart=/bin/bash -c 'source ${HOME}/.bash_profile && ${PWD}/iris-exporter -masterIP=192.168.10.76 -listen=:9102 -irisBinPath=${HOME}/IRIS/bin/'
 PermissionsStartOnly=true
-ExecStartPre=/bin/mkdir -p /var/log/iris-exporter
-ExecStartPre=/bin/chmod 755 /var/log/iris-exporter
 StandardOutput=syslog
 StandardError=syslog
 SyslogIdentifier=iris-exporter
